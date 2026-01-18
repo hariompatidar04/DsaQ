a93d32fad94a88df5fbd8fc298bfa873c69815d8
hariompatidar04 <patidarh930@gmail.com>
2026-01-18 11:30:44 +0530

    Solve Sum of all Array Element with Optimal way

--- SumOFElement.java
+++ SumOFElement.java
@@ -1,18 +1,25 @@
-class SumOFElement{
 
-  static int sumOfEle(int [] arr)
-  {
-    int sum=0;
-    for(int i=0;i<arr.length;i++)
-    {
-      sum+=arr[i];
+class SumOFElement {
+
+    static int sumOfEle(int[] arr) {
+        int sum = 0;
+
+        //for loop
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //   sum+=arr[i];
+        // }
+
+        //enhanced for loop 
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
     }
-    return sum;
-  }
-  public static void main(String[] args)
-  {
-    int [] arr={1,2,3,4,5,6,7,8,9,10};
-    int res=sumOfEle(arr);
-    System.out.println("Sum of all elements in the array: "+res);
-  }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int res = sumOfEle(arr);
+        System.out.println("Sum of all elements in the array: " + res);
+    }
 }
